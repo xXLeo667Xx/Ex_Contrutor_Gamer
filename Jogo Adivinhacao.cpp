213ea20593fa11535cf889cb499c1e1f4dce71db
xXLeo667Xx <127798966+xXLeo667Xx@users.noreply.github.com>
2023-05-08 02:04:12 -0300

    Update Jogo Adivinhacao.cpp

--- Jogo Adivinhacao.cpp
+++ Jogo Adivinhacao.cpp
@@ -3,41 +3,48 @@
 #include<cstdlib>
 using namespace std;
 //criando a classe e colocando a variaveis
-class JogoAdivinhacao{
-    //colocando variaveis em privado 
-    private:
-    int NumeroSecreto;
-    int tentativa;
-    //publico para usuario 
-    public:
-    JogoAdivinhacao(){
-        //sortiar um numero de 1 a 100
-        srand(time(0));
-        NumeroSecreto=rand() %100 + 1;
-        tentativa=0;
-    }
-    //e  metodo para se jogar o jogo, e as interface para o jogo funcionar 
-    void Jogar(){
-        int Palpite;
-            do{
-                cout<<"digite um numero de 1 a 100: ";
-                cin>>Palpite;
-                tentativa++;
-                if(Palpite>NumeroSecreto){
-                   cout<<"O numero secreto é menor!"<<endl;
-                }else if(Palpite<NumeroSecreto){
-                   cout<<"O numero secreto é maior!"<<endl;
-                }else{
-                   cout<<"Parabens voce acertou o numero em "<<tentativa<<" Tentantivas!"<<endl;
-               }
-               //para repetir ate o usuario acertar o numero 
-            }while(Palpite!=NumeroSecreto);
-        }
-};
-//programa pricipal para ativar o jogo 
 int main(){
-    JogoAdivinhacao Jogo;
-    Jogo.Jogar();
-    return 0;
+    //pra saber quantas vezes poderar repitir
+    const int MaxTentativas = 5;
+    //variaveis
+    int numero, palpite, nivel, tentativas=0;
+    //demarcando as dificudades, 3 filas de dificuldades com 2 numeros cada
+    int intervalos[3][2]={{1, 10},{1, 50},{1, 100}};
+    bool acertou = false
+    //sortiar um numero aleatorio
+    srand(time(NULL));
+    //interface para dificudade
+    cout<<"bem-vindo ao jogo de advinhação\n";
+    cout<<"escolha o nivel de dificudade\n";
+    cout<<"1- facil(1 a 10)\n";
+    cout<<"2- medio(1 a 50)\n";
+    cout<<"3- dificil (1 a 100)\n";
+    cin>>nivel;
+    //caso ele coloque uma dificuldade acima do que a permitida
+    if(nivel<1||nivel>3){
+        cout<<"opção invalida\n";
+        return 0;
+    }
+    //numero sortiado
+    numero=rand() % (intervalos[nivel-1][1] - intervalos[nivel-1][0] + 1)=intervalos[nivel-1][0];
+    //repeticao com contador de tentativas
+    while(!acertou && tentativas<MaxTentativas){
+        cout<<"digite um palpite entre "<<intervalos[nivel-1][0]<<"e"<<intervalos[nivel-1][1]<<":\n";
+        cin>>palpite;
+        tentativas++;
+        //caso vc erre dizer se o numero sorteado e maior ou menor ou se acertou
+        if(palpite<numero){
+            cout<<"o numero é maior.\n";
+        }else if(palpite>numero){
+            cout<<"o numero é menor.\n";
+        }else{
+            acertou=true;
+            cout<<"parabens vocé acertou em "<<tentativas<<"tentantivas!\n";
+        }
+        //caso tenha acabado suas tentativas
+        if(!acertou){
+            cout<<"vocé perdeu! o numero era "<<numero<<".\n";
+        }
+        return 0;
+    }
 }
-/*ele verifica as bibliotecas, criamos  uma classe aonde temos os atributos e as variaveis para poder o jogo funcionar, colocamos o numero aleatorio que puxa de 1 a 100,depois da classe criada criamos o metodo colocando variavel e fazemos a interface do ususario para ele tentar adivinhar o numero, e o jogo vai dando dica ate ele acerta, depois temos o codigo se repeticao para repetir o erro ate acertar contabilizando as tentantivas, depois temos o programa principal aonde colocamos o sistema da classe e do metodo funcionarem juntos e rodar o jogo/*
